9a87bd16b1955073e19a943304c6da791961a1ad
10ADM01 <ADITYAMISHRATN@GMAIL.COM>
2023-09-19 19:35:14 +0530

    1

---
+++ src/datatypes.java
@@ -0,0 +1,10 @@
+public class datatypes {
+    public static void main(String[] args) {
+        byte  x =   8;
+        System.out.println("range of byte -128 to 127");
+        short x1 = 23;
+        System.out.println("range of short -32768 to 32767");
+
+
+    }
+}
