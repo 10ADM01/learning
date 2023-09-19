fc486203d548b43f2ca85106b6f8a97c0159c56f
10ADM01 <ADITYAMISHRATN@GMAIL.COM>
2023-09-19 20:14:56 +0530

    1

--- src/precedence.java
+++ src/precedence.java
@@ -1,5 +1,6 @@
 public class precedence {
     public static void main(String[] args) {
-
+        //go to this for precedence
+        System.out.println("https://www.youtube.com/watch?v=vh16INx13h4&list=PLu0W_9lII9agS67Uits0UnJyrYiXhDS6q&index=9");
     }
 }
